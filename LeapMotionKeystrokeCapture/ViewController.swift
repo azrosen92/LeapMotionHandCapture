6cdf876d6f24d8cdfc0ba2228deb52732f3b695e
azrosen92 <azrosen92@gmail.com>
2017-05-15 17:07:04 -0400

    Initial Commit

---
+++ LeapMotionKeystrokeCapture/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LeapMotionKeystrokeCapture
+//
+//  Created by Aaron Rosen on 5/15/17.
+//  Copyright © 2017 Aaron Rosen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
